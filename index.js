0db6c6b7796c1e29caa642916bff51512b1ef793
agusjoffre <aguslj16@gmail.com>
2023-11-21 04:23:19 -0300

    addinfomodule. not sure

--- index.js
+++ index.js
@@ -26,51 +26,95 @@ class Note {
 
 // create info module
 
-const creationModule = (() => {
+const createInfoModule = (() => {
   // Projects array
   const projects = [];
   // Project details array
   const projectDetails = [];
   // Project creation fn
-  function createProject(title, _details) {
+  const createProject = (title, _details) => {
     const newProject = new Project(title, _details);
     projects.push(newProject);
-  }
+  };
 
   // ToDos array
   const toDos = [];
   // ToDo creation fn
-  function createToDo(title, description, dueDate, notes, checklist) {
+  const createToDo = (title, description, dueDate, notes, checklist) => {
     const newTodo = new Todo(title, description, dueDate, notes, checklist);
     toDos.push(newTodo);
     projectDetails.push(newTodo);
-  }
+  };
   // Notes array
   const notes = [];
   // Notes creation fn
-  function createNote(title, _details) {
+  const createNote = (title, _details) => {
     const newNote = new Note(title, _details);
     notes.push(newNote);
     projectDetails.push(newNote);
-  }
+  };
 
-  return { projectDetails, projects, createToDo, createNote, createProject };
+  return {
+    projectDetails,
+    projects,
+    createToDo,
+    createNote,
+    createProject,
+    toDos,
+    notes,
+  };
 })();
 
-//--------------------------------------testing----------------------------------------------------//
-let note1 = creationModule.createNote("Test Note", "Test Note Details...");
-let todo1 = creationModule.createToDo(
+// add Info module fn
+
+const addInfoModule = (() => {
+  // NO PUEDO PONER _DETAILS COMO PARAMETRO PQ EN REALIDAD SON TODO Y NOTE.
+  const addProject = (_title) => {
+    createInfoModule.createProject(_title);
+  };
+  const addToDo = (_title, _description, _dueDate, _notes, _checklist) => {
+    if (_title && _description && _dueDate && _notes && _checklist) {
+      createInfoModule.createToDo(
+        _title,
+        _description,
+        _dueDate,
+        _notes,
+        _checklist
+      );
+    }
+  };
+  const addNote = (_title, _details) => {
+    if (_title) {
+      createInfoModule.createNote(_title, _details);
+    }
+  };
+  return { addProject, addToDo, addNote };
+})();
+
+//----------------------------testing--------------------------------------------//
+let note1 = createInfoModule.createNote("Test Note", "Test Note Details...");
+let todo1 = createInfoModule.createToDo(
   "Test title",
   "test description",
   "24/12/23",
   "Test notes",
   true
 );
-
-creationModule.createProject(
+createInfoModule.createProject(
   "Test project title",
-  creationModule.projectDetails
+  createInfoModule.projectDetails
 );
 
-console.log(creationModule.projects);
-//---------------------------------------------------------------------------------------------------//
+let project2 = addInfoModule.addProject("Project 2");
+let todo2 = addInfoModule.addToDo(
+  "Todo 2",
+  "Description 2",
+  "23/12/23",
+  "Notes 2",
+  true
+);
+let note2 = addInfoModule.addNote("Note 2", "Details 2");
+console.log(createInfoModule.projects);
+console.log(createInfoModule.toDos);
+console.log(createInfoModule.notes);
+//-------------------------------------------------------------------------------//
